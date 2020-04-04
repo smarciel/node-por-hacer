fe3327909cfa4c8ef8839a2c687acd394aaa804c
smarciel <sarah.marciel@gmail.com>
2020-04-04 12:05:42 +0200

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,23 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+    demand: true,
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crea un elemento por hacer', { descripcion })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', { descripcion, completado })
+    .command('borrar', 'Borra una tarea', { descripcion })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('database/data.json', data, (err) => {
+        if (err) {
+            throw new Error('Ha habido un error');
+        }
+    });
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../database/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const crear = (desc) => {
+    cargarDB();
+    let porHacer = {
+        desc,
+        completado: false
+    };
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+
+const actualizar = (desc, completado = true) => {
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.desc === desc);
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (desc) => {
+    cargarDB();
+    let listadoSinTarea = listadoPorHacer.filter(tarea => tarea.desc !== desc);
+    if (listadoSinTarea.length !== listadoPorHacer.length) {
+        listadoPorHacer = listadoSinTarea;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
